1b0df713327e44242883452eff21382ace80d29b
4lch4 <git@4lch4.email>
2023-02-07 16:42:37 -0600

    refactor: moved intro info to AboutBrief component

---
+++ src/content/constants/About.ts
@@ -0,0 +1,14 @@
+/** My primary online name/alias. */
+export const PrimaryAlias = '4lch4'
+
+/** My real name, including first, middle initial, and last. */
+export const RealName = 'Devin W. Leaman'
+
+/** My "personal" job title, not my official title at my current gig. */
+export const JobTitle = 'Sr. Full Stack Engineer / Sr. Operations Engineer'
+
+/** A brief bio about me. It's the same one I use across all my profiles. */
+export const BioBrief = "I'm just some guy who likes making and fixing things that usually involve a keyboard and/or a mouse."
+
+/** A URL to my 4lch4.link website, which provides links to all my profiles. */
+export const LinksURL = 'https://4lch4.link'
