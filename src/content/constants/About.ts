adb62918ab353f7a73082bc987a7fd0a68b5496a
4lch4 <git@4lch4.email>
2023-02-08 12:04:09 -0600

    feat: added start of About page

--- src/content/constants/About.ts
+++ src/content/constants/About.ts
@@ -10,5 +10,17 @@ export const JobTitle = 'Sr. Full Stack Engineer / Sr. Operations Engineer'
 /** A brief bio about me. It's the same one I use across all my profiles. */
 export const BioBrief = "I'm just some guy who likes making and fixing things that usually involve a keyboard and/or a mouse."
 
+/** 
+ * A longer description of me, my interests, my goals, etc., in under 500
+ * characters. It'll appear just under the {@link BioBrief} text on the
+ * homepage.
+ */
+export const BioLong = `
+Jokes aside, I list my title as ${JobTitle} because of my current role at my
+current job at J.B. Hunt. 
+
+I'm a full time Sr. Operations Engineer by title, but I'm also a Sr. Full Stack Engineer based on my responsibilities and the work I do. I graduated in the Spring of 2015 with a B.S in Information Technology - Programming, and started my first job at SAPIEN Technologies, Inc. in the Fall of 2015. I have been working steadily in the industry as a software engineer since then and began my Operations Engineer journey when I started at J.B. Hunt in 2019. 
+`
+
 /** A URL to my 4lch4.link website, which provides links to all my profiles. */
 export const LinksURL = 'https://4lch4.link'
---
+++ src/content/constants/Site.ts
@@ -0,0 +1,5 @@
+/** The title of the website. */
+export const SITE_TITLE = "4lch4's Homepage"
+
+/** The description of the website. */
+export const SITE_DESCRIPTION = '4lch4.com — The homepage, blog, portfolio, etc. of 4lch4.'
